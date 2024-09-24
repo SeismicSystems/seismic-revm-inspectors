a3898be7dc36b021a0a355418fcbb12b9bca5404
phexyz <32248504+phexyz@users.noreply.github.com>
2024-09-24 17:34:22 -0400

    fixed build

--- src/tracing/mux.rs
+++ src/tracing/mux.rs
@@ -240,6 +240,10 @@ impl DelegatingInspector {
 
                 Ok(DelegatingInspector::Prestate(prestate_config, inspector))
             }
+            GethDebugBuiltInTracerType::FlatCallTracer => {
+                // TODO support flat call tracer in mux
+                return Err(Error::UnexpectedConfig(tracer_type));
+            }
             GethDebugBuiltInTracerType::NoopTracer => {
                 if tracer_config.is_some() {
                     return Err(Error::UnexpectedConfig(tracer_type));
